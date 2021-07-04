8f76524fb278513ad134ca29633abe8b08743160
MehdiT-dev <84857223+MehdiT-dev@users.noreply.github.com>
2021-07-04 23:16:33 +0200

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const btnNav = document.getElementById('btn-nav');
-const navigation = document.getElementById('navigation');
-const ulNavigation = document.querySelector('#navigation ul');
-
-btnNav.addEventListener('click', () => {
-    btnNav.classList.toggle('btn-active');
-    navigation.classList.toggle('nav-active');
-});
